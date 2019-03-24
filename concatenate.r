dba5797e0993e9dee48b9c5468a79963390a8be2
Paulina Penroz <35982394+PaPenroz@users.noreply.github.com>
2019-03-23 22:20:24 -0300

    Create concatenate.r

---
+++ concatenate.r
@@ -0,0 +1,3 @@
+1:5 + 6:10 #Suma de vectores. Suma el primer número del lado izquierdo, con el primer número del lado derecho. Resultado: [1] 7 9 11 13 15
+
+c(1, 3, 6, 10, 15) + c(0, 1, 3, 6, 10) #La función 'c' ayuda a concatenar set de números variados y así crear vectores. Resultado: [1] 1 4 9 16 25
