ffb08db517694ffe819b2a3b4feebf5af4065874
Paulina Penroz <35982394+PaPenroz@users.noreply.github.com>
2019-03-23 23:17:58 -0300

    Update calculator.r

--- calculator.r
+++ calculator.r
@@ -1,3 +1,21 @@
+# SUMA
+
 1:5 + 6:10 #Suma de vectores. Suma el primer número del lado izquierdo, con el primer número del lado derecho. Resultado: [1] 7 9 11 13 15
 
 c(1, 3, 6, 10, 15) + c(0, 1, 3, 6, 10) #La función 'c' ayuda a concatenar set de números variados y así crear vectores. Resultado: [1] 1 4 9 16 25
+
+sum(1:5) #Suma todos los valores contenidos en el vector entre ellos. Resultado: 15
+
+
+# RESTA
+
+c(2, 3, 5, 7, 11, 13) - 2 #Resta de los valores. Resultado: [1] 0 1 3 5 9 11
+
+
+# MULTIPLICACIÓN
+
+-2:2 * -2:2 #Multiplicación de vectores. Resultado: [1] 4 1 0 1 4
+
+
+# EXPONENCIA se puede ingresar tanto con un '^' como con '**'
+
